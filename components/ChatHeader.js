8147127d9d3173f93ec7120087a197048e32ae58
Mutaawe Enock <mutaawe.enock@chordify.com>
2025-07-17 16:58:37 +0300

    Remove anonymous user indication from ChatHeader component for a cleaner UI

--- components/ChatHeader.js
+++ components/ChatHeader.js
@@ -29,11 +29,6 @@ const ChatHeader = () => {
           <span className="inline-flex items-center px-2 py-0.5 rounded-full text-xs font-medium bg-purple-100 text-purple-800 dark:bg-purple-900 dark:text-purple-200">
             v3.2
           </span>
-          {!user && (
-            <span className="inline-flex items-center px-2 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-600 dark:bg-gray-700 dark:text-gray-300">
-              Anonymous
-            </span>
-          )}
         </div>
       </div>
       
